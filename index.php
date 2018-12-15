9d967542dbc2768d8d58b5b88c10a34d4318d529
Nikita Yurkin <inkover@ya.ru>
2018-12-15 17:50:18 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Remon Versteeg
+ * Date: 11-9-2018
+ * Time: 20:21
+ */
+
+
+
+
+require_once __DIR__ . "/autoloader.php";
+
+
+$url = 'https://www.audi-boerse.de/gebrauchtwagen/audi/q8/q8/wuppertal/diesel/automatik/286ps-allradantrieb-silber-2_DEU2185285215842NW.htm#/i/s|10,AAEO/l|12,1,STAT_GWPLUS,U';
+
+
+$scraper = new \Imod\Scraper\Audi();
+var_dump($scraper->getDetails($url));
---
+++ src/Scraper/Porsche.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Remon Versteeg
+ * Date: 14-12-2018
+ * Time: 10:35
+ */
+
+
+namespace Scraper;
+
+class Porsche
+{
+
+
+
+    public function getPages(){
+
+
+
+
+    }
+
+    public function getDetails( $url ){
+
+
+
+    }
+
+
+}
